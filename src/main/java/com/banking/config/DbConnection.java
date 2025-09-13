c400d430e123d1fefaba4db8fcd95aed0ea77b6a
ray-cod <raimilassissi8@gmail.com>
2025-09-13 11:35:18 +0200

    Created DbConnection class.

---
+++ src/main/java/com/banking/config/DbConnection.java
@@ -0,0 +1,16 @@
+package com.banking.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DbConnection {
+    private static String url = "jdbc:h2:~/atmDb;AUTO_SERVER=TRUE";
+    private static String username = "sa";
+    private static String password = "";
+
+    public static void getConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection(url, username, password);
+    }
+}
